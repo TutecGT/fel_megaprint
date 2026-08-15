ab3b122f5b4085d3834c762ced4ec9dfa00d9c9b
fnisthal <fnisthal@gmail.com>
2026-08-15 12:44:44 -0600

    Mejora diagnóstico de respuestas del certificador

--- models/account.py
+++ models/account.py
@@ -78,6 +78,7 @@ class AccountMove(models.Model):
                             data = '<?xml version="1.0" encoding="UTF-8"?><RegistraDocumentoXMLRequest id="{}"><xml_dte><![CDATA[{}]]></xml_dte></RegistraDocumentoXMLRequest>'.format(uuid_factura, xml_con_firma)
                             logging.warning(data)
                             r = requests.post('https://'+request_url+'.ifacere-fel.com/'+request_path+'api/registrarDocumentoXML', data=data.encode('utf-8'), headers=headers)
+                            logging.warning(r.text)
                             resultadoXML = etree.XML(bytes(r.text, encoding='utf-8'))
 
                             if len(resultadoXML.xpath("//listado_errores")) == 0:
@@ -99,7 +100,9 @@ class AccountMove(models.Model):
                                 if len(resultadoXML.xpath("//listado_errores")) == 0:
                                     pdf = resultadoXML.xpath("//pdf")[0].text
                                     factura.pdf_fel = pdf
-                                    
+                                else:
+                                    factura.error_certificador(r.text)
+
                             else:
                                 factura.error_certificador(r.text)
 
